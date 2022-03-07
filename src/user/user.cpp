73d744f7144ba42060ba138f5c8a44ea75f68afb
Antero Marjamäki <ehtycs.github@gmail.com>
2022-03-07 12:10:38 +0200

    Implemented getXscrdata and setXcsrdata

--- src/user/user.cpp
+++ src/user/user.cpp
@@ -1,35 +1,8 @@
 #include "user.h"
 
-/** 
- * Get a copy of the data in the underlying PETSC vector of 
- * a sl::vec.  E.g. for python world.
- * FIXME: seems that this is not the right way to go for multiple 
- * processors but will do for now.
+/*
+ * Local functions
  */
-std::vector<double> getvecdata(vec &slvector) {
-
-    double *p = NULL;
-    // Just read only access, no VecRestoreArray needed
-    VecGetArray(slvector.getpetsc(), &p);
-    std::vector<double> out(p, p+slvector.size());
-    return out;
-}
-
-void setvecdata(vec &slvector, std::vector<double> &values) {
-    if(slvector.size() != values.size()) {
-        std::stringstream tmp;
-        tmp << "The size of the sl::vec " << slvector.size() 
-            << " and the size of the values " << values.size()
-            << " don't match.";
-        throw std::runtime_error(tmp.str());
-    }
-    double* p = NULL;
-    VecGetArray(slvector.getpetsc(), &p);
-    for (int i = 0; i < values.size(); i++) {
-        p[i] = values[i];
-    }
-    VecRestoreArray(slvector.getpetsc(), &p);
-}
 
 /** 
  *  Extract the CSR data of the matrix to transfer it to python and 
@@ -38,9 +11,7 @@ void setvecdata(vec &slvector, std::vector<double> &values) {
  * "Inspired" by PETSc4py function getValuesCSR:
  *  https://gitlab.com/petsc/petsc/-/blob/main/src/binding/petsc4py/src/PETSc/Mat.pyx#L911
  */
-csr_data getcsrdata(mat &slmat) {
-
-    Mat pmat = slmat.getapetsc();
+csr_data _getcsrdata(Mat pmat) {
 
     // row ownership
     int rstart = 0, rend=0, nrows=0;
@@ -76,13 +47,82 @@ csr_data getcsrdata(mat &slmat) {
         MatRestoreRow(pmat, irow+rstart, &ncols, &cols, &vals);
     }
 
-    return {ai, aj, av};
+    // return as (data, indices, indptr)
+    return {av, aj, ai};
 
 }
 
-void setcsrdata(mat &slmat, std::vector<int> &rowi, 
+/*
+ *  Set data to PETSc matrix such that pmat[rowi[i], coli[i]] = vals[i]
+ *  Inputs:
+ *     rowi - row indices
+ *     coli - column indices
+ *     vals - values
+ */
+void _setcsrdata(Mat pmat, std::vector<int> &rowi, 
                 std::vector<int> &coli, std::vector<double> &vals) {
+    
+    if(rowi.size() != coli.size() || coli.size() != vals.size()) {
+        std::stringstream tmp;
+        tmp << "The sizes of the row and column indices and the "
+            << "value vector must match"; 
+        throw std::runtime_error(tmp.str());
+    }
+        
+    for(int i = 0; i < vals.size(); i++) {
+        MatSetValue(pmat, rowi[i], coli[i], vals[i], INSERT_VALUES);
+    }
 
-    std::cout << "I'm here" << std::endl;
+    MatAssemblyBegin(pmat, MAT_FINAL_ASSEMBLY);
+    MatAssemblyEnd(pmat, MAT_FINAL_ASSEMBLY);
+}
 
+
+/** 
+ * Get a copy of the data in the underlying PETSC vector of 
+ * a sl::vec.  E.g. for python world.
+ * FIXME: seems that this is not the right way to go for multiple 
+ * processors but will do for now.
+ */
+std::vector<double> getvecdata(vec &slvector) {
+
+    double *p = NULL;
+    // Just read only access, no VecRestoreArray needed
+    VecGetArray(slvector.getpetsc(), &p);
+    std::vector<double> out(p, p+slvector.size());
+    return out;
+}
+
+void setvecdata(vec &slvector, std::vector<double> &values) {
+    if(slvector.size() != values.size()) {
+        std::stringstream tmp;
+        tmp << "The size of the sl::vec " << slvector.size() 
+            << " and the size of the values " << values.size()
+            << " don't match.";
+        throw std::runtime_error(tmp.str());
+    }
+    double* p = NULL;
+    VecGetArray(slvector.getpetsc(), &p);
+    for (int i = 0; i < values.size(); i++) {
+        p[i] = values[i];
+    }
+    VecRestoreArray(slvector.getpetsc(), &p);
+}
+
+csr_data getacsrdata(mat &slmat) {
+    return _getcsrdata(slmat.getapetsc());
+}
+
+csr_data getdcsrdata(mat &slmat) {
+    return _getcsrdata(slmat.getdpetsc());
+}
+
+void setacsrdata(mat &slmat, std::vector<int> &rowi, 
+                std::vector<int> &coli, std::vector<double> &vals) {
+    _setcsrdata(slmat.getapetsc(), rowi, coli, vals);
+}
+
+void setdcsrdata(mat &slmat, std::vector<int> &rowi, 
+                std::vector<int> &coli, std::vector<double> &vals) {
+    _setcsrdata(slmat.getdpetsc(), rowi, coli, vals);
 }
--- src/user/user.h
+++ src/user/user.h
@@ -2,12 +2,16 @@
 #include "vec.h"
 #include "mat.h"
 
-typedef std::tuple<std::vector<int>,std::vector<int>,std::vector<double>> csr_data;
+typedef std::tuple<std::vector<double>,std::vector<int>,std::vector<int>> csr_data;
 
 
 std::vector<double> getvecdata(vec &slvector);
 void setvecdata(vec &slvector, std::vector<double> &values);
 
-csr_data getcsrdata(mat &slmat);
-void setcsrdata(mat &slmat, std::vector<int> &rowi, 
+csr_data getacsrdata(mat &slmat);
+void setacsrdata(mat &slmat, std::vector<int> &rowi, 
+                std::vector<int> &coli, std::vector<double> &vals);
+
+csr_data getdcsrdata(mat &slmat);
+void setdcsrdata(mat &slmat, std::vector<int> &rowi, 
                 std::vector<int> &coli, std::vector<double> &vals);
